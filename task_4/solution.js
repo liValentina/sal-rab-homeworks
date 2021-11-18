8d8cbc4da4812a68a0ff2a194676c936c75b7feb
Valentina Manuylova <manuylova.va@yandex.ru>
2021-11-18 22:18:43 +0300

    task_4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,16 +1,28 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
+    
+    let data = JSON.parse(json);
+    let products = data.products;
+    return products
     // Аргументом функции является JSON
-
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
-    // и запишите в переменную data
-    // Верните как результат функции свойство products объекта data
+    // и запишите в переменную data  
+    // Верните как результат функции ПЕРЕМЕННУЮ (https://github.com/netology-ds-team/sal-rab-homeworks/blob/master/task_4/readme.md) 
+    //products объекта data
 }
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
+function renderProductsCards(json){
+ clearProducts();  
+ let products =  parseProducts(json);
+ let length = products.length
+ for (let i = 0; i < length; i += 1) {
+     addProduct(products[i]);
+ }
+};
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
 // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
 // Запишите в переменную length значение свойства products.length
